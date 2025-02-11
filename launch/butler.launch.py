c10678486309f5bd1a8ea1d618a016ff034d1853
Bhajneet Singh Bedi <bhajneetbedi2002@gmail.com>
2025-02-12 01:56:26 +0530

    first commit

---
+++ launch/butler.launch.py
@@ -0,0 +1,61 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument, 
+    SetEnvironmentVariable, 
+    IncludeLaunchDescription, 
+    RegisterEventHandler,
+    ExecuteProcess
+)
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import (
+    PathJoinSubstitution, 
+    LaunchConfiguration, 
+    TextSubstitution
+)
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+import xacro
+
+def generate_launch_description():
+
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    pkg_omni_wheel_bot = get_package_share_directory('autonomous_butler')
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    # urdf_file = os.path.join(get_package_share_directory('autonomous_butler'), 'urdf', 'butler.urdf')  
+    urdf_file = os.path.join(get_package_share_directory('autonomous_butler'), 'model', 'model.sdf')
+    assert os.path.exists(urdf_file), "The butler.urdf doesnt exist in "+str(urdf_file)  
+    urdf = open(urdf_file).read()
+
+
+
+
+    DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='If true, use simulated clock'),
+
+
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={
+            'gz_args': ' -v4 -r /home/bhajneet/ros2_ws/src/autonomous_butler/model/model.sdf'
+        }.items(),
+    )
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/model/vehicle_blue/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+                   '/model/vehicle_blue/odometry@nav_msgs/msg/Odometry@gz.msgs.Odometry',
+                   'lidar@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'],
+        parameters=[{'qos_overrides./model/vehicle_blue.subscriber.reliability': 'reliable'}],
+        output='screen'
+    )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    return LaunchDescription([
+        gz_sim,
+        bridge
+    ])
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+package_name = 'autonomous_butler'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+	    (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+	    (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+	    (os.path.join('share', package_name, 'model'), glob('model/*')),
+
+
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='bhajneet',
+    maintainer_email='bhajneetbedi2002@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)

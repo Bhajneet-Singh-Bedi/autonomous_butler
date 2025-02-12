9badf28ab38367058bb8ec0fcfd06f4f42a022ee
Bhajneet Singh Bedi <bhajneetbedi2002@gmail.com>
2025-02-12 20:32:41 +0530

    [Bhajneet] Added config, rviz and changed gazebo.world

--- launch/butler.launch.py
+++ launch/butler.launch.py
@@ -1,77 +1,177 @@
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument, 
-    SetEnvironmentVariable, 
-    IncludeLaunchDescription, 
-    RegisterEventHandler,
-    ExecuteProcess
-)
-from launch.event_handlers import OnProcessExit
-from launch.substitutions import (
-    PathJoinSubstitution, 
-    LaunchConfiguration, 
-    TextSubstitution
-)
+import launch
 from launch_ros.actions import Node
+from launch.actions import (
+    ExecuteProcess,
+    DeclareLaunchArgument,
+    LogInfo,
+    RegisterEventHandler,
+    TimerAction,
+)
+from launch.conditions import IfCondition
+from launch.substitutions import (
+    LaunchConfiguration,
+    PathJoinSubstitution,
+    NotSubstitution,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-import os
-import xacro
+from launch_ros.substitutions import FindPackageShare
+from launch.events.process import ProcessIO
+from launch.event_handlers import OnProcessIO
+
+# Create event handler that waits for an output message and then returns actions
+def on_matching_output(matcher: str, result: launch.SomeActionsType):
+    def on_output(event: ProcessIO):
+        for line in event.text.decode().splitlines():
+            if matcher in line:
+                return result
+
+    return on_output
+
+
+# Lanch the robot and the navigation stack
+
 
 def generate_launch_description():
-
-    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    pkg_omni_wheel_bot = get_package_share_directory('autonomous_butler')
-    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-
-    # urdf_file = os.path.join(get_package_share_directory('autonomous_butler'), 'urdf', 'butler.urdf')  
-    urdf_file = os.path.join(get_package_share_directory('autonomous_butler'), 'model', 'model.sdf')
-    assert os.path.exists(urdf_file), "The butler.urdf doesnt exist in "+str(urdf_file)  
-    urdf = open(urdf_file).read()
-
-
-
-
-    DeclareLaunchArgument(
-            'use_sim_time',
-            default_value=use_sim_time,
-            description='If true, use simulated clock'),
-
-
-    gz_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
-        launch_arguments={
-            'gz_args': ' -v4 -r /home/bhajneet/ros2_ws/src/autonomous_butler/worlds/gazebo.sdf'
-        }.items(),
+    # Messages are from: https://navigation.ros.org/setup_guides/sensors/setup_sensors.html#launching-nav2
+    diff_drive_loaded_message = (
+        "Successfully loaded controller diff_drive_base_controller into state active"
     )
+    toolbox_ready_message = "Registering sensor"
+    navigation_ready_message = "Creating bond timer"
 
+    run_headless = LaunchConfiguration("run_headless")
 
-    spawn_entity = Node(
-        package="ros_gz_sim",
-        executable="create",
-        name="spawn_sdf_entity",
-        output="screen",
-        arguments=[
-            "-file", urdf_file,   # Path to the SDF file
-            "-name", "robot_name",    # Name of the robot to be spawned
-            "-x", "0.0",                # X position
-            "-y", "0.0",                # Y position
-            "-z", "0.5"                 # Z position
+    # Including launchfiles with execute process because i didn't find another way to wait for a certain messages befor starting the next launchfile
+    bringup = ExecuteProcess(
+        name="launch_bringup",
+        cmd=[
+            "ros2",
+            "launch",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("autonomous_butler"),
+                    "launch",
+                    "display.launch.py",
+                ]
+            ),
+            "use_rviz:=false",
+            ["run_headless:=", run_headless],
+            "use_localization:=false",
         ],
+        shell=False,
+        output="screen",
     )
-    # Bridge
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
-                   'odom@nav_msgs/msg/Odometry@gz.msgs.Odometry',
-                   'scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'],
-        parameters=[{'qos_overrides./model/vehicle_blue.subscriber.reliability': 'reliable'}],
-        output='screen'
-    )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-    return LaunchDescription([
-        gz_sim,
-        spawn_entity,
-        bridge
-    ])
+    toolbox = ExecuteProcess(
+        name="launch_slam_toolbox",
+        cmd=[
+            "ros2",
+            "launch",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("slam_toolbox"),
+                    "launch",
+                    "online_async_launch.py",
+                ]
+            ),
+        ],
+        shell=False,
+        output="screen",
+    )
+    waiting_toolbox = RegisterEventHandler(
+        OnProcessIO(
+            target_action=bringup,
+            on_stdout=on_matching_output(
+                diff_drive_loaded_message,
+                [
+                    LogInfo(
+                        msg="Diff drive controller loaded. Starting SLAM Toolbox..."
+                    ),
+                    toolbox,
+                ],
+            ),
+        )
+    )
+
+    navigation = ExecuteProcess(
+        name="launch_navigation",
+        cmd=[
+            "ros2",
+            "launch",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("nav2_bringup"),
+                    "launch",
+                    "navigation_launch.py",
+                ]
+            ),
+            "use_sim_time:=True",
+            ["params_file:=", LaunchConfiguration('params_file')]
+        ],
+        shell=False,
+        output="screen",
+    )
+    rviz_node = Node(
+        condition=IfCondition(NotSubstitution(run_headless)),
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", LaunchConfiguration("rvizconfig")],
+    )
+    waiting_navigation = RegisterEventHandler(
+        OnProcessIO(
+            target_action=toolbox,
+            on_stdout=on_matching_output(
+                # diff_drive_loaded_message,
+                toolbox_ready_message,
+                [
+                    LogInfo(msg="SLAM Toolbox loaded. Starting navigation..."),
+                    # TODO Debug: Navigation fails to start if it's launched right after the slam_toolbox
+                    TimerAction(
+                        period=20.0,
+                        actions=[navigation],
+                    ),
+                    rviz_node,
+                ],
+            ),
+        )
+    )
+
+    waiting_success = RegisterEventHandler(
+        OnProcessIO(
+            target_action=navigation,
+            on_stdout=on_matching_output(
+                navigation_ready_message,
+                [
+                    LogInfo(msg="Ready for navigation!"),
+                ],
+            ),
+        )
+    )
+
+    return launch.LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "params_file",
+                default_value=[FindPackageShare("autonomous_butler"), "/config/nav2_params.yaml"],
+                description="Full path to the ROS2 parameters file to use for all launched nodes",
+            ),
+            DeclareLaunchArgument(
+                name="rvizconfig",
+                default_value=[
+                    FindPackageShare("autonomous_butler"),
+                    "/rviz/navigation_config.rviz",
+                ],
+                description="Absolute path to rviz config file",
+            ),
+            DeclareLaunchArgument(
+                name="run_headless",
+                default_value="False",
+                description="Start GZ in hedless mode and don't start RViz (overrides use_rviz)",
+            ),
+            bringup,
+            waiting_toolbox,
+            waiting_navigation,
+            waiting_success,
+        ]
+    )

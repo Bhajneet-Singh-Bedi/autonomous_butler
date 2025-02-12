741d8f4e4f0b181cf8c307cb849dff0b0ba95cce
Bhajneet Singh Bedi <bhajneetbedi2002@gmail.com>
2025-02-12 11:57:01 +0530

    [Bhajneet] Separated world and model. Added scene in world file.

--- launch/butler.launch.py
+++ launch/butler.launch.py
@@ -42,8 +42,23 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
         launch_arguments={
-            'gz_args': ' -v4 -r /home/bhajneet/ros2_ws/src/autonomous_butler/model/model.sdf'
+            'gz_args': ' -v4  /home/bhajneet/ros2_ws/src/autonomous_butler/worlds/gazebo.sdf'
         }.items(),
+    )
+
+
+    spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        name="spawn_sdf_entity",
+        output="screen",
+        arguments=[
+            "-file", urdf_file,   # Path to the SDF file
+            "-name", "robot_name",    # Name of the robot to be spawned
+            "-x", "0.0",                # X position
+            "-y", "0.0",                # Y position
+            "-z", "0.5"                 # Z position
+        ],
     )
     # Bridge
     bridge = Node(
@@ -57,5 +72,6 @@ def generate_launch_description():
     )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
     return LaunchDescription([
         gz_sim,
+        spawn_entity,
         bridge
     ])
